49c0a2fbd264fc887567959a27327039036f2de9
Ria Dharnidharka <rdharni@emory.edu>
2024-02-12 13:09:13 -0500

    commit

--- submission1/data-code/H1_HCRISv1996.R
+++ submission1/data-code/H1_HCRISv1996.R
@@ -38,8 +38,8 @@ colnames(hcris.vars)=c("variable","WKSHT_CD","LINE_NUM","CLMN_NUM","source")
 ## -- note: v1996 of HCRIS forms run through 2011 due to lags in processing and hospital
 ##    fiscal years
 ########################################################################################
-# update this for years change 1998
-for (i in 1998:2011) {
+# update this for years change 1998 to 2008
+for (i in 2008:2011) {
   HCRIS.alpha=read_csv(paste0("data/input/HCRIS_v1996/HospitalFY",i,"/hosp_",i,"_ALPHA.CSV"),
                        col_names=c('RPT_REC_NUM','WKSHT_CD','LINE_NUM','CLMN_NUM','ITM_VAL_NUM'))
   HCRIS.numeric=read_csv(paste0("data/input/HCRIS_v1996/HospitalFY",i,"/hosp_",i,"_NMRC.CSV"),
@@ -68,11 +68,11 @@ for (i in 1998:2011) {
               by="report")
   }
   assign(paste("final.reports.",i,sep=""),final.reports)
-  if (i==1998) {
+  if (i==2008) {
     final.hcris.v1996=final.reports.1998
   } else {
     final.hcris.v1996=rbind(final.hcris.v1996,get(paste("final.reports.",i,sep="")))
   }
-  #change for years 
+  
 }
 write_rds(final.hcris.v1996,'data/output/HCRIS_Data_v1996.rds')
--- submission1/data-code/_HCRIS_Data.R
+++ submission1/data-code/_HCRIS_Data.R
@@ -10,8 +10,8 @@ pacman::p_load(tidyverse, ggplot2, dplyr, lubridate)
 
 
 # Read and combine data ---------------------------------------------------
-source('data-code/H1_HCRISv1996.R')
-source('data-code/H2_HCRISv2010.R')
+source('submission1/data-code/H1_HCRISv1996.R')
+source('submission1/data-code/H2_HCRISv2010.R')
 
 final.hcris.v1996=read_rds('data/output/HCRIS_Data_v1996.rds')
 final.hcris.v2010=read_rds('data/output/HCRIS_Data_v2010.rds')
